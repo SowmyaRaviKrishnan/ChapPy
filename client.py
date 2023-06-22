1d59654b5962d5210b47ede7b6aea8e17793b74a
Sowmya R <sowmyr44@in.ibm.com>
2023-06-22 15:56:53 +0530

    changes for rpc

--- client.py
+++ client.py
@@ -10,7 +10,10 @@ def request_to_send_authenticate(peer_ip,peer_port):
 if __name__ == "__main__":
       while True:
             for peer in peers:
-                request_to_send_authenticate(peers[peer].ip,peers[peer].port)
+                connection = connect_peer(peers[peer].ip,peers[peer].port)
+                nonce,nextnonce = connection.root.authenticate(current_node.username,current_node.response,current_node.cnonce)
+                current_node.response = current_node.calculate_response(nonce)
+                current_node.cnonce = current_node.calculate_cnonce(nextnonce)
             sleep(10)
             
 
--- peer.py
+++ peer.py
@@ -16,10 +16,17 @@ class Node:
 class Peers:
     def __init__(self):
         self.peers = {}
-    
+        r1 = Node("10.0.0.1",118811,"r1")
+        r2 = Node("10.0.0.2",118811,"r2")
+        r2 = Node("10.0.1.1",118811,"r2")
+        r3 = Node("10.0.1.2",118811,"r3")
+        self.add_peer("r1",r1)
+        self.add_peer("r3",r3)
+
     def add_peer(self,username,node):
         self.peers[username] = node
 
     def get_peers(self):
         return self.peers
+
 
