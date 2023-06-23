8e95e0beb1d48e675631b102e8db3c615da6b73d
Sowmya R <sowmyr44@in.ibm.com>
2023-06-23 19:32:42 +0530

    changes

--- User.py
+++ User.py
@@ -10,34 +10,3 @@ class User:
         self.response  = None 
         self.message = None
     
-"""
-def putuser(user):
-    print(user.username,user.nonce,user.nextnonce,user.message)    
-#user creation
-user = User("test")
-pychap.authenticate(putuser,user)
-
-#1st auth
-print("second try")
-
-
-
-
-
-pychap.authenticate(putuser,user)
-
-#2nd auth
-
-print("third try")
-
-response = hashlib.sha1(user.nonce.encode('ascii'))
-response.update("password".encode('ascii'))
-user.response = response.hexdigest()
-
-
-cnonce = hashlib.sha1(user.nextnonce.encode('ascii'))
-cnonce.update("password".encode('ascii'))
-user.cnonce = hashlib.sha1(cnonce.hexdigest().encode("ascii")).hexdigest()
-
-pychap.authenticate(putuser,user)
-"""
--- utils.py
+++ utils.py
@@ -6,14 +6,15 @@ r1 = Node("10.0.0.1",118811,"r1")
 r1_r2 = Node("10.0.0.2",118811,"r2")
 r3_r2 = Node("10.0.1.1",118811,"r2")
 r3 = Node("10.0.1.2",118811,"r3")
+r4 = Node("10.0.2.2",118811,"r4")
 
 peers_list = [r1,r1_r2,r3_r2,r3]
 
 
-r2_self = SelfNode("r2","password","10.0.0.2","r2",118811,peers_list)
-r1_self = SelfNode("r1","password","10.0.0.1","r1",118811,peers_list)
-r3_self = SelfNode("r3","password","10.0.1.2","r3",118811,peers_list)
-r4_self = SelfNode("r4","password","10.0.2.2","r4",118811,peers_list)
+r2_self = SelfNode("r2","r2password","10.0.0.2","r2",118811,peers_list)
+r1_self = SelfNode("r1","r1password","10.0.0.1","r1",118811,peers_list)
+r3_self = SelfNode("r3","r3password","10.0.1.2","r3",118811,peers_list)
+r4_self = SelfNode("r4","r4password","10.0.2.2","r4",118811,peers_list)
 current_node = r1_self 
 peers = Peers(peers_list)
 
