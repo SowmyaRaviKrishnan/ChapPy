563ad8d5d45bfbeb2c8acbf271167e90ca13bcce
Sowmya R <sowmyr44@in.ibm.com>
2023-06-23 08:44:36 +0530

    changes to selfnode typo

--- utils.py
+++ utils.py
@@ -10,8 +10,8 @@ r3 = Node("10.0.1.2",118811,"r3")
 peers_list = [r1,r1_r2,r3_r2,r3]
 
 
-r1_self = SelfNode("r2","password","10.0.0.2","r2",118811,peers_list)
-r2_self = SelfNode("r1","password","10.0.0.1","r1",118811,peers_list)
+r2_self = SelfNode("r2","password","10.0.0.2","r2",118811,peers_list)
+r1_self = SelfNode("r1","password","10.0.0.1","r1",118811,peers_list)
 r3_self = SelfNode("r3","password","10.0.1.2","r3",118811,peers_list)
 current_node = r1_self 
 peers = Peers(peers_list)
